88c53a611e14bfff45eada0cc8993f841faae6a1
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-06-11 20:51:38 -0300

    :bug: fix: now user is unable to select a completed task

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -8,9 +8,12 @@ interface ItemProps {
 
 export default function Item({task, selectTask}: ItemProps) {
   return (
-    <li className={`${style.item} ${task.selected ? style.itemSelecionado : ''} ${task.completed ? style.itemCompletado : ''}`} onClick={() => selectTask(task)}>
+    <li className={`${style.item} ${task.selected ? style.itemSelecionado : ''} ${task.completed ? style.itemCompletado : ''}`} onClick={() => !task.completed && selectTask(task)}>
       <h3 >{task.task_name}</h3>
       <span>{task.time}</span>
+      {task.completed && (
+        <span className={style.concluido} aria-label="tarefa completada"></span>
+      )}
     </li>
   );
 }
